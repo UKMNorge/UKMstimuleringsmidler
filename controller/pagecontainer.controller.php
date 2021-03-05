855939b383f664d5429582ce6e3143dba987e0cb
slogan-15 <kushtrimaliu19@gmail.com>
2021-03-05 10:31:27 +0100

    Sider fra WP

---
+++ controller/pagecontainer.controller.php
@@ -0,0 +1,14 @@
+<?php
+// echo 'test';
+require_once( UKMwp_innhold::getPath() .'functions/getPage.function.php');
+
+$PAGE_SLUG = str_replace('UKMstimulering_', '', $_GET['page']);
+
+if( isset( $_GET['subpage'] ) ) {
+    $PAGE_SLUG = $PAGE_SLUG .'/'. $_GET['subpage'];
+}
+
+UKMstimuleringsmidler::addViewData(
+	'post', 
+	getPage('stimuleringsmidler/' . $PAGE_SLUG)
+);
